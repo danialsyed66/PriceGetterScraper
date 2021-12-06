11d8b302eff245cf2863bd26fa892f5b8fa8528b
danialsyed66 <danialsyed66@gmail.com>
2021-12-06 15:24:53 +0500

    home fix

--- scraper/daraz/scrapeUrls.js
+++ scraper/daraz/scrapeUrls.js
@@ -3,10 +3,42 @@ const cheerio = require("cheerio");
 
 module.exports = async (puppeteerPage, products, url, category) => {
   try {
-    const html = await puppeteerPage.evaluate(() => document.body.innerHTML);
-    const $ = await cheerio.load(html);
+    for (let page = 1; page < 2; page++) {
+      console.log(`Category: ${category}, Page: ${page}`);
 
-    products.push({ html });
+      await puppeteerPage.goto(
+        queryString.stringifyUrl({
+          url,
+          query: {
+            page,
+            q: category,
+          },
+        }),
+        { waitUntil: "networkidle2" }
+      );
+
+      const html = await puppeteerPage.evaluate(() => document.body.innerHTML);
+      const $ = await cheerio.load(html);
+
+      // const arr = $("a")
+      //   .map((i, el) => "https:" + $(el).attr("href"))
+      //   .get();
+
+      // const set = arr.map((url) => {
+      //   if (url.startsWith("https://www.daraz.pk/products")) return url;
+      // });
+
+      // const urls = [...new Set([...set])];
+
+      // console.log(
+      //   `Category: ${category}, Page: ${page}: Got ${urls.length} urls`
+      // );
+
+      // urls.map((url) => {
+      //   products.push({ url, category, seller: "Daraz" });
+      // });
+      products.push({ html });
+    }
   } catch (err) {
     throw err;
   }
